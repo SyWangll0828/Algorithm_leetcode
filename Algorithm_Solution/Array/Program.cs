7ac549956caa102309bf2f66c153429c301719e6
王磊磊 <wangleilei@herenit.com>
2021-06-15 20:25:44 +0800

    二分查找第一天 初认知

--- Algorithm_Solution/Array/Program.cs
+++ Algorithm_Solution/Array/Program.cs
@@ -18,8 +18,9 @@ namespace Array
             //Console.WriteLine(string.Join("", testNums.ToArray()));
             //problems.IntToRoman(testCase.MyProperty2);
             Knowledge.Rank(3, testCase.index);
+            int[] test = new int[] { 3, 1 };
             //var rcptnos = new List<string>();
-            //problems.Test(rcptnos);
+            problems.Search(test, 1);
             //var n = problems.TwoSum2(new int[] { 2, 2, 7, 11, 15 }, 100);
             //problems.LengthOfLongestSubstring("pwwkew");
             //problems.ContainsNearbyDuplicate(testCase.nums, 1);
@@ -77,11 +78,16 @@ namespace Array
 
         #region 二分查找
         //模板
+        //left<right
+        //左边区间：将数组分割为[left,mid-1]和[mid,right]
+        //需要上取整- mid=left+(right-left+1)/2; 
+        //left=mid;right=mid-1;
+        //右边区间：将数组分割为[left,mid]和[mid+1,right]
+        //left=mid+1;right=mid;
 
         //167. 两数之和 II - 输入有序数组
         public int[] TwoSum2(int[] numbers, int target)
         {
-            //二分查找
             //for (int i = 0; i < numbers.Length; ++i)
             //{
             //    int low = i + 1, high = numbers.Length - 1;
@@ -117,6 +123,129 @@ namespace Array
         //{
 
         //}
+        //374. 猜数字大小
+        //public int GuessNumber(int n)
+        //{
+        //    int left = 1, right = n;
+        //    while (left <= right)
+        //    {
+        //        int mid = (right - left) / 2 + left;
+        //        //所选数字在[mid,right]之间
+        //        if (guess(mid) <= 0)
+        //            right = mid - 1;
+        //        //所选数字在[left,mid]之间
+        //        else
+        //            left = mid + 1;
+        //    }
+        //    return left;
+        //}
+        //278. 第一个错误的版本
+        //public int FirstBadVersion(int n)
+        //{
+        //    int left = 1, right = n;
+        //    while (left < right)
+        //    { // 循环直至区间左右端点相同
+        //        int mid = left + (right - left) / 2; // 防止计算时溢出
+        //        if (IsBadVersion(mid))
+        //            right = mid; // 答案在区间 [left, mid] 中
+        //        else
+        //            left = mid + 1; // 答案在区间 [mid+1, right] 中
+        //    }
+        //    // 此时有 left == right，区间缩为一个点，即为答案
+        //    return left;
+        //}
+        //852. 山脉数组的峰顶索引(找到数组中最大值的下标)
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            int left = 0, right = arr.Length - 1;
+            while (left < right)
+            { // 循环直至区间左右端点相同
+                int mid = left + (right - left) / 2; // 防止计算时溢出
+                if (arr[mid] > arr[mid + 1])
+                    right = mid; // 答案在区间 [left, mid] 中
+                else
+                    left = mid + 1; // 答案在区间 [mid+1, right] 中
+            }
+            // 此时有 left == right，区间缩为一个点，即为最大值的下标
+            return left;
+
+        }
+        //633. 平方数之和
+        public bool JudgeSquareSum(int c)
+        {
+            int left = 0, right = (int)Math.Sqrt(c);
+            while (left <= right)
+            {
+                int res = left * left + right * right;
+                if (res == c)
+                    return true;
+                else if (res > c)
+                    right--;
+                else
+                    left++;
+            }
+            return false;
+        }
+        //34. 在排序数组中查找元素的第一个和最后一个位置
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int[] res = new int[] { -1, -1 };
+            if (nums.Length == 0 || nums == null) return res;
+
+            res[0] = binarySearch(nums, target, true);
+            res[1] = binarySearch(nums, target, false);
+            return res;
+        }
+
+        public int binarySearch(int[] nums, int target, bool flag)
+        {
+            int n = nums.Length;
+            int left = 0, right = n - 1;
+            int ans = -1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > target)
+                    right = mid - 1;
+                else if (nums[mid] < target)
+                    left = mid + 1;
+                else
+                {
+                    ans = mid;
+                    if (flag) right = mid - 1;
+                    else left = mid + 1;
+                }
+            }
+            return ans;
+        }
+        //33. 搜索旋转排序数组
+        public int Search(int[] nums, int target)
+        {
+            int len = nums.Length;
+            if (len == 0) return -1;
+            int left = 0, right = len - 1;
+            while (left < right)
+            {
+                int mid = left + (right - left + 1) / 2;
+                if (nums[mid] == target) return mid;
+                if (nums[mid] > nums[left])
+                {
+                    if (target >= nums[left] && target <= nums[mid - 1])//将数组分割为[left,mid-1]和[mid,right]
+                        right = mid - 1;
+                    else
+                        left = mid;
+                }
+                else
+                {
+                    if (target >= nums[mid] && target <= nums[right])
+                        left = mid;
+                    else
+                        right = mid - 1;
+                }
+            }
+            return nums[left] != target ? -1 : left;
+        }
         #endregion
 
         #region 滑动窗口
